8087d3668ca3bab86e6144b83962fa290bded761
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-02-11 16:53:06 +0100

    Request with validated data to register new user is working

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,10 +1,21 @@
 import Debug from 'debug'
 import { Request, Response } from 'express'
+import {matchedData, validationResult} from "express-validator";
 
 const debug = Debug('prisma-photos:user_controller')
 
 // Register a new user
 export const register = async (req:Request, res:Response) => {
     const { email, password } = req.body
-    console.log("req.body: ", email, password)
+
+    const validationErrors = validationResult(req)
+    if(!validationErrors.isEmpty()) {
+        return res.status(400).send({
+            status: "fail",
+            data: validationErrors.array()
+        })
+    }
+
+    const validatedData = matchedData(req)
+    console.log("Validated data: ", validatedData)
 }
--- src/validations/user_validation.ts
+++ src/validations/user_validation.ts
@@ -1,13 +1,14 @@
 // Validation of user
 // Import modules
 import { body } from 'express-validator'
+import { validationResult } from "express-validator";
 
 // Import source
 import { getUserByEmail } from "../services/user_service";
 
 export const registerUserValidation = [
     body('first_name').isString().bail().isLength({ min: 3 }),
-    body('first_name').isString().bail().isLength({ min: 3 }),
+    body('last_name').isString().bail().isLength({ min: 3 }),
     body('email').isEmail().bail().custom(async (value:string) => {
         const user = await getUserByEmail(value)
 
