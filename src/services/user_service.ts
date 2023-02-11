a584e785c91086c38584b0d32e7ed43088708ccc
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-02-11 16:42:23 +0100

    Validation and first routes done. Next is to setup Postman with some test data

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -4,4 +4,15 @@ import prisma from "../prisma";
 // Import source
 import { registerUser } from "../types/user";
 
+// POST
 // Register new user
+
+// GET
+// Get user by email
+export const getUserByEmail = async (email:string) => {
+    return prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+}
--- src/validations/user_validation.ts
+++ src/validations/user_validation.ts
@@ -1,7 +1,19 @@
 // Validation of user
+// Import modules
 import { body } from 'express-validator'
 
+// Import source
+import { getUserByEmail } from "../services/user_service";
 
 export const registerUserValidation = [
-    console.log("Validation of user")
+    body('first_name').isString().bail().isLength({ min: 3 }),
+    body('first_name').isString().bail().isLength({ min: 3 }),
+    body('email').isEmail().bail().custom(async (value:string) => {
+        const user = await getUserByEmail(value)
+
+        if(user) {
+            return Promise.reject("User already exist")
+        }
+    }),
+    body('password').isString().bail().isLength({ min: 6 })
 ]
